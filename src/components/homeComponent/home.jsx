a4d465a53361351519e3b1eb2f7d27a8769c8d06
JasperC <jchancoding@gmail.com>
2019-03-20 22:51:29 -0700

    Initial commit of header component

--- src/components/homeComponent/home.jsx
+++ src/components/homeComponent/home.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
+import ReactDOM from 'react-dom';
 import { Link } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
 
 export default class Home extends Component {
   render() {
     return (
       <div>
         <h1>We HOME</h1>
+        <Button variant="contained" color="primary">
+          Hello World
+        </Button>
       </div>
     )
   }
