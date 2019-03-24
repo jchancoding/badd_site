e3533acc7a69c8c66651027aba05e09d0ffd3c69
JasperC <jchancoding@gmail.com>
2019-03-24 12:34:16 -0700

    Saving changes

--- src/components/headerComponent/header.jsx
+++ src/components/headerComponent/header.jsx
@@ -1,7 +1,7 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom';
+import React, {Component} from 'react'
+import {Link} from 'react-router-dom';
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
+import {withStyles} from '@material-ui/core/styles';
 
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -19,93 +19,103 @@ import MenuList from '@material-ui/core/MenuList';
 
 const styles = {
     root: {
-      flexGrow: 1,
+        flexGrow: 1
     },
     grow: {
-      flexGrow: 1,
+        flexGrow: 1
     },
     menuButton: {
-      marginLeft: -12,
-      marginRight: 20,
-    },
-  };
+        marginLeft: -12,
+        marginRight: 20
+    }
+};
 
-class Header extends React.Component {  
+class Header extends React.Component {
     state = {
-        open: false,
+        open: false
     };
 
     handleToggle = () => {
-        this.setState(state => ({ open: !state.open }));
-      };
-    
-      handleClose = event => {
+        this.setState(state => ({
+            open: !state.open
+        }));
+    };
+
+    handleClose = event => {
         if (this.anchorEl.contains(event.target)) {
-          return;
+            return;
         }
-    
-        this.setState({ open: false });
-      };
+
+        this.setState({open: false});
+    };
 
     render() {
-        const { classes } = this.props;
-        const { open } = this.state;
+        const {classes} = this.props;
+        const {open} = this.state;
         const navBar = (
             <div className={classes.root}>
-              <AppBar position="static">
-                <Toolbar>
-                  <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
-                    <MenuIcon />
-                  </IconButton>
-                  <Typography variant="h6" color="inherit" className={classes.grow}>
-                    BADD
-                  </Typography>
-                  <Button color="inherit"
-                    buttonRef={node => {
-                    this.anchorEl = node;
-                    }}
-                    aria-owns={open ? 'menu-list-grow' : undefined}
-                    aria-haspopup="true"
-                    onClick={this.handleToggle}
-                    >
-                    Pop Menu
-                  </Button>
-                  <Popper open={open} anchorEl={this.anchorEl} transition disablePortal>
-                    {({ TransitionProps, placement }) => (
-                        <Grow
-                            {...TransitionProps}
-                            id="menu-list-grow"
-                            style={{ transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom' }}
-                            >
-                            <Paper>
-                            <ClickAwayListener onClickAway={this.handleClose}>
-                                <MenuList>
-                                <MenuItem onClick={this.handleClose}>Profile</MenuItem>
-                                <MenuItem onClick={this.handleClose}>My account</MenuItem>
-                                <MenuItem onClick={this.handleClose}>Logout</MenuItem>
-                                </MenuList>
-                            </ClickAwayListener>
-                            </Paper>
-                        </Grow>
-                        )}
-                    </Popper>
-                    <Button color="inherit">Gallery</Button>
-                    <Button color="inherit">Maps</Button>
-                </Toolbar>
-              </AppBar>
+                <AppBar position="static">
+                    <Toolbar>
+                        <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+                            <MenuIcon/>
+                        </IconButton>
+                        <Typography variant="h6" color="inherit" className={classes.grow}>
+                            BADD
+                        </Typography>
+                        <Button
+                            color="inherit"
+                            buttonRef={node => {
+                            this.anchorEl = node;
+                        }}
+                            aria-owns={open
+                            ? 'menu-list-grow'
+                            : undefined}
+                            aria-haspopup="true"
+                            onClick={this.handleToggle}>
+                            Pop Menu
+                        </Button>
+                        <Popper open={open} anchorEl={this.anchorEl} transition disablePortal>
+                            {({TransitionProps, placement}) => (
+                                <Grow
+                                    {...TransitionProps}
+                                    id="menu-list-grow"
+                                    style={{
+                                    transformOrigin: placement === 'bottom'
+                                        ? 'center top'
+                                        : 'center bottom'
+                                }}>
+                                    <Paper>
+                                        <ClickAwayListener onClickAway={this.handleClose}>
+                                            <MenuList>
+                                                <MenuItem onClick={this.handleClose}>Profile</MenuItem>
+                                                <MenuItem onClick={this.handleClose}>My account</MenuItem>
+                                                <MenuItem onClick={this.handleClose}>Logout</MenuItem>
+                                            </MenuList>
+                                        </ClickAwayListener>
+                                    </Paper>
+                                </Grow>
+                            )}
+                        </Popper>
+                        <Button color="inherit">Calendar</Button>
+                        <Button color="inherit">Gallery</Button>
+                        <Button color="inherit">Maps</Button>
+                        <Button color="inherit">Reading</Button>
+                        <Button color="inherit">Recs</Button>
+                    </Toolbar>
+                </AppBar>
             </div>
         );
 
-    return (
-      <div>
-        {navBar}
-      </div>
-    )
-  }
+        return (
+            <div>
+                {navBar}
+            </div>
+        )
+    }
 }
 
 Header.propTypes = {
-    classes: PropTypes.object.isRequired,
-  };
-  
-  export default withStyles(styles)(Header);
+    classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(Header);
--- src/components/homeComponent/home.jsx
+++ src/components/homeComponent/home.jsx
@@ -1,17 +1,44 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import ReactDOM from 'react-dom';
-import { Link } from 'react-router-dom';
-import Button from '@material-ui/core/Button';
+import {Link} from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-export default class Home extends Component {
-  render() {
-    return (
-      <div>
-        <h1>We HOME</h1>
-        <Button variant="contained" color="primary">
-          Hello World
-        </Button>
-      </div>
-    )
-  }
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+class Home extends Component {
+    
+    render() {
+      const { classes } = this.props;
+        return (
+            <div>
+                <Paper>
+                    <Grid container>
+                        <Grid item md={6}>
+                            <div>
+                                <Typography component="h1" variant="h3" color="inherit" gutterBottom>
+                                    Bay Area Discord Drivers
+                                </Typography>
+                                <Typography variant="h5" color="inherit" paragraph>
+                                    Welcome to the bay area car enthusiast hub!
+                                </Typography>
+                            </div>
+                        </Grid>
+                    </Grid>
+                </Paper>
+                <h1>We HOME</h1>
+                <Button variant="contained" color="primary">
+                    Hello World
+                </Button>
+            </div>
+        )
+    }
 }
+
+Home.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default Home;
